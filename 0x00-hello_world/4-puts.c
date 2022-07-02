4f11eae912acdb029252361c8e7ad8ffa11062fa
Oyewole Tosin <Oyewoletosinayodele7348@gmail.com>
2022-07-02 13:52:42 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a string with puts
+ *
+ * Return: 0 if program executed successfully
+ */
 int main(void)
-{ 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
