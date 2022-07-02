ccd0b88c23e685d685dc9d01254be4849539f0bc
Oyewole Tosin <Oyewoletosinayodele7348@gmail.com>
2022-07-02 14:03:46 -0700

    initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main (void) {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);	
+/**
+ * main - Prints a string with printf
+ *
+ * Return: 0 if program executed successfully
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
 }
